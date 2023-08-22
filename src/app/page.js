74734746613f053c999fb4b208ac6d77a0083061
snehasish's stuff <snehasishcodes@gmail.com>
2023-08-22 11:32:45 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -8,13 +8,7 @@ const kanit = Kanit({ subsets: ['latin'], weight: '400' });
 export default function Home() {
 	return (
 		<>
-			<Flex direction={'column'} justify={'center'} align={'center'} style={{ overflow: 'scroll' }}>
-				<Flex direction={'row'} gap={'2'} justify={'center'}>
-					<Image src={'/icon.png'} height={50} width={50} />
-					<Text className={kanit.className} size={'7'} style={{ lineHeight: '200%'}}>PERIODIC TABLE</Text>
-				</Flex>
-				<Table />
-			</Flex>
+			<Table />
 		</>
 	)
 }
